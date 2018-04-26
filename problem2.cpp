e1fae199ac158a95589e2640801cca457f0a1768
Valentino <38631625+pytagora@users.noreply.github.com>
2018-04-27 01:24:16 +0200

    Update problem2.cpp

--- problem2.cpp
+++ problem2.cpp
@@ -9,15 +9,21 @@ int main(){
 	unsigned long int sum = 2;
 	unsigned long int tmp;
 	unsigned long int maximum = 4000000;
+	// init of vector and pushing first two fib numbers
 	std::vector <unsigned long int> v;
 	v.push_back(1);
 	v.push_back(2);
+	// declaration of vector iterator
+	unsigned long int i = 0;
 	do{
-		tmp = v[0] + v[1];
+		tmp = v[i] + v[i + 1];
+		// adding new fib number
 		v.push_back(tmp);
+		// checking if it is even
 		if (tmp % 2 == 0)
 			sum += tmp;
-		v.erase(v.begin());
+		// incrementating iterator
+		++i;
 	} while(tmp < maximum);
 	std::cout << sum << std::endl;
 	return 0;
