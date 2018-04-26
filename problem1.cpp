ecd41be7c4ea5b91b6eeb7e4695f851806389632
Valentino <38631625+pytagora@users.noreply.github.com>
2018-04-27 01:18:03 +0200

    Update problem1.cpp

--- problem1.cpp
+++ problem1.cpp
@@ -6,7 +6,9 @@
 int main()
 {
     long int sum = 0;
+    // iterating integers below 1000
     for (int i = 1; i < 1000; ++i){
+        // checking multiplicity
         if (i % 3 == 0 || i % 5 == 0) 
             sum += i;
     }
