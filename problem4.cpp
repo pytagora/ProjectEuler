f8245a84d24c1e0850ebbdbe4b2de66c010c6b4c
Valentino <38631625+pytagora@users.noreply.github.com>
2018-04-27 01:37:09 +0200

    Update problem4.cpp

--- problem4.cpp
+++ problem4.cpp
@@ -5,14 +5,18 @@
 #include <iostream>
 #include <vector>
 
+// function for checking is the argument palindrome
 bool IsPalindrome(unsigned long int num, std::vector<int> v)
 {
+    // pushing each integer into vector
     while (num > 0){
         v.push_back(num % 10);
         num /= 10;
     }
     int s = v.size();
+    // iterating the number
     for (int j = 0; j < s; ++j){
+        // checking the palindrome condition
         if (v[j] != v[s - 1 - j])
             return false;
     }
@@ -23,6 +27,7 @@ int main()
 {
     std::vector <int> v;
     int limit = 1000;
+    // result
     int maximum = 1;
     int tmp;
     for (int i = 3; i < limit; ++i){
